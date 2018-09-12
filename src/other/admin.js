c6c64a84ea921c2f5a7c364a015cd850f33e43b7
theyworker <devaka.kumarasinghe@gmail.com>
2018-09-12 07:02:57 +0530

    Admin Dashboard showing cars

--- src/other/admin.js
+++ src/other/admin.js
@@ -9,10 +9,19 @@ App = {
         carDisplayTemp.find('.panel-title').text(data[i].name);
         carDisplayTemp.find('img').attr('src', data[i].picture);
         carDisplayTemp.find('.car-Manufacturer').text(data[i].Manufacturer);
-        carDisplayTemp.find('.car-yearofmanu').text(data[i].yearofmanu);
+        var salests;
+        if(data[i].ForSale){
+          salests = "On Sale";
+        }
+        else {
+          salests =  "SOLD";
+        }
+        carDisplayTemp.find('.car-sts').text(salests);
         carDisplayTemp.find('.car-EngineNumber').text(data[i].EngineNumber);
         carDisplayTemp.find('.btn-buy').attr('data-id', data[i].id);
         carDisplayTemp.find('.btn-md').attr('data-id', data[i].id);
+
+
 
         petsRow.append(carDisplayTemp.html());
 
